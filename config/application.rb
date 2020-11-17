98c887de0df520fe7a244c455e7b2153f1e95f7d
Ehugo2000 <erik.hgsn@gmail.com>
2020-11-17 14:41:37 +0100

    Project setup. Installing Gemfiles and updating Ruby v.

---
+++ config/application.rb
@@ -0,0 +1,34 @@
+require_relative 'boot'
+
+require "rails"
+
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+
+Bundler.require(*Rails.groups)
+
+module CooperApi
+  class Application < Rails::Application
+
+    config.load_defaults 5.1
+
+    config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', 
+          headers: :any, 
+          methods: %i[get post put delete],
+          expose: %w(access-token expiry token-type uid client),
+          max_age: 0
+      end
+    end
+  end
+end
+ 
