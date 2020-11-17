bfe249bb4217a767f4ebc5198dade06db3043e8e
Ehugo2000 <erik.hgsn@gmail.com>
2020-11-17 16:39:14 +0100

    Add db:migrate, Fbot for user, specs for model_user, all tests works

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  extend Devise::Models
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable
+  include DeviseTokenAuth::Concerns::User
+end
