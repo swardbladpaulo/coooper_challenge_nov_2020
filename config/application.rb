77e0d41a36b3a9f26b480bdfc78fa4db34e0e06c
swardbladpaulo <paulo@swardblads.se>
2020-11-17 19:47:03 +0100

    update application.rb, routes.rb, rails_helper.rb and spec_helper.rb

--- config/application.rb
+++ config/application.rb
@@ -31,4 +31,3 @@ module CooperApi
     end
   end
 end
- 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip: [:omniauth_callbacks]
   namespace :api do
-    namespace :v0 do
-      resources :pings, only: [:index], constraints: { format: 'json' }
-     namespace :v1 do
-     end
+      namespace :v0 do
+        resources :pings, only: [:index], constraints: { format: 'json' }
+      namespace :v1 do
+      end
     end
   end
 end
