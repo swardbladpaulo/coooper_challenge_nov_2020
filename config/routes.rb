24851b8697b060ce640c05207c8bb6ab40adeade
Ehugo2000 <erik.hgsn@gmail.com>
2020-11-18 11:40:13 +0100

    Adding index routes and testing for GET and POST

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip: [:omniauth_callbacks]
   namespace :api do
-      #namespace :v0 do
-       # resources :pings, only: [:index], constraints: { format: 'json' }
-      namespace :v1, defaults: { format: :json } do
-        resources :performance_data, only: [:create]
+       namespace :v1, defaults: { format: :json } do
+        resources :performance_data, only: [:create, :index]
       end
-    #end
-  end
+    end
 end
--- spec/request/api/v1/performance_data_spec.rb
+++ spec/request/api/v1/performance_data_spec.rb
@@ -23,4 +23,26 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
       expect(entry.data).to eq 'message' => 'Average'
     end
   end
+
+  describe 'GET /api/v1/performance_data' do
+    let!(:existing_entries) do
+      5.times do
+        create(:performance_data,
+              data: { message: 'Average' },
+              user: user)
+      end
+    end
+
+    before do
+      get '/api/v1/performance_data', headers: headers
+    end
+
+    it 'returns a 200 response status' do
+      expect(response).to have_http_status 200
+    end
+
+    it 'returns a collection of performance data' do
+      expect(response_json['entries'].count).to eq 5
+    end
+  end
 end
